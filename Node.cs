bb40fc666f6e5b5da85fde2541a606261c13adc9
bored-user <joao.guimaraes.alves.vaz@gmail.com>
2020-10-10 19:24:16 -0300

    Rewritten the whole file. Kinda working now

--- Node.cs
+++ Node.cs
@@ -1,29 +1,60 @@
-using System.Threading.Tasks;
+using System;
+using System.Collections.Generic;
 
 namespace Snake
 {
     class Node
     {
-        public Directions Direction { get; set; }
-        public (int X, int Y) Coordinates = (1, 0);
-        public ((int X, int Y) Coordinates, Directions Direction) ChangeDirCoord { get; set; }
+        public List<((int X, int Y) Coordinates, Directions direction)> Change = new List<((int X, int Y) Coordinates, Directions direction)>();
+        private static List<Node> NodeList = new List<Node>();
+        public Directions Direction { get; private set; }
+        public (int X, int Y) Coordinates;
+        public char Char
+        {
+            get => this.Direction == Directions.Left || this.Direction == Directions.Right ? '▬' : '▮';
+        }
 
-        public Node(Directions direction)
+        public Node(Directions direction, (int X, int Y) coordinates)
         {
             this.Direction = direction;
+            this.Coordinates = coordinates;
+
+            Node.NodeList.Add(this);
         }
 
-        public async void Move()
+        public void Move()
         {
-            await Task.Run(() => {
-                while(true)
-                {
-                    if(this.Coordinates != this.ChangeDirCoord.Coordinates) continue;
-                    this.Direction = this.ChangeDirCoord.Direction;
-                    return;
-                }
-            });
+            this.Direction = this.GetNewDirection() ?? this.Direction;
+
+            if (this.Direction == Directions.Up)
+                this.Coordinates.Y -= Snake.Unit;
+            else if (this.Direction == Directions.Down)
+                this.Coordinates.Y += Snake.Unit;
+            else if (this.Direction == Directions.Left)
+                this.Coordinates.X -= Snake.Unit;
+            else
+                this.Coordinates.X += Snake.Unit;
         }
 
+        private Directions? GetNewDirection()
+        {
+            int idx;
+
+            if (this.Direction == Directions.Up || this.Direction == Directions.Down)
+                idx = this.Change.FindIndex((((int X, int Y) Coordinates, Directions direction) change) => (change.Coordinates.Y == this.Coordinates.Y));
+            else
+                idx = this.Change.FindIndex((((int X, int Y) Coordinates, Directions direction) change) => (change.Coordinates.X == this.Coordinates.X));
+
+            if (idx > -1)
+            {
+                Directions direction = this.Change[idx].direction;
+                this.Change.RemoveAt(idx);
+                return direction;
+            };
+
+            return null;
+        }
+
+        public static bool IsThereANodeHere((int X, int Y) coordinates) => Node.NodeList.Exists(n => n.Coordinates.X == coordinates.X && n.Coordinates.Y == coordinates.Y);
     }
 }
--- Snake.cs
+++ Snake.cs
@@ -1,84 +1,125 @@
 using System;
+using System.Linq;
 using System.Collections.Generic;
 
 namespace Snake
 {
-    enum Directions
-    {
-        Up = 1,
-        Down = -1,
-        Left = 2,
-        Right = -2
-    }
-
     class Snake
     {
-        private (int X, int Y) _coordinates = (1, 0);
-        public int Size
-        {
-            get => this._body.Count;
-            private set
-            {
-                if (value < this._body.Count) return;
-                value = value - this._body.Count;
-                for (int i = 0; i < value; i++)
-                    this.Grow();
-            }
-        }
-        public readonly int FrameRate = 250;
-        private readonly List<Node> _body;
-        public Directions HeadDirection
-        {
-            get => this._body.Count < 1 ? Directions.Right : this._body[0].Direction;
-            private set => this._body[0].Direction = value;
-        }
+        private (int X, int Y) _coordinates = (Console.WindowWidth / 2, Console.WindowHeight / 2);
+        private List<Node> _body = new List<Node>();
+        public static int Unit = 1 + (2 - 1) * (Console.WindowHeight - 1) / (Console.WindowWidth - 1);
+        public Directions Direction { get; private set; } = Directions.Right;
+        public int Size { get => this._body.Count; }
+        public int FrameRate = 500;
 
         public Snake()
         {
-            this._coordinates = (Console.WindowWidth / 2, Console.WindowHeight / 2);
-            this._body = new List<Node>();
-            this.Size = 3;
+            this.Grow();
+            this.Grow();
+            this.Grow();
         }
 
-        public void Move(Directions direction)
+        public void Move(Directions direction, bool isDefaultForce = false)
         {
-            if (this.HeadDirection == direction || (int)this.HeadDirection + (int)direction == 0) return;
+            // If the snake's head is touching any borders
+            // OR
+            // its own tale
+            if (this._coordinates.X == Console.WindowWidth || this._coordinates.X == 0 || this._coordinates.Y == Console.WindowHeight || this._coordinates.Y == 0 || Node.IsThereANodeHere(this._coordinates))
+                Program.Quit("YOU LOST!");
 
-            this.HeadDirection = direction;
-            int d = (int)this.HeadDirection;
+            // If the player is trying to go to the opposite direction (right - left, up - down)
+            // OR
+            // If the current direction is the same as the new direction
+            // AND
+            // This movement isn't the default force
+            if ((int)this.Direction + (int)direction == 0 || this.Direction == direction && !isDefaultForce) return;
 
-            if (d % 2 != 0)
-                this._coordinates.Y -= d;
+            // If the new direction is UP, remove one unit on Y
+            // If it's DOWN, add one unit on Y
+            // If it's LEFT, remove one unit on X
+            // If it's RIGHT, add one unit on X
+            if (direction == Directions.Up)
+                this._coordinates.Y -= Snake.Unit;
+            else if (direction == Directions.Down)
+                this._coordinates.Y += Snake.Unit;
+            else if (direction == Directions.Left)
+                this._coordinates.X -= Snake.Unit;
             else
-                this._coordinates.X -= d == 2 ? 1 : -1;
+                this._coordinates.X += Snake.Unit;
 
+            // Setting the new direction;
+            // For every node, add the new direction to the direction-coordinate change list
+            // (if it's not the same direction the node is going to) and move the nodes
+            this.Direction = direction;
             this._body.ForEach(n =>
             {
-                n.ChangeDirCoord = n.Direction != direction ? (this._coordinates, direction) : n.ChangeDirCoord;
-                if (d % 2 != 0)
-                    n.Coordinates.Y -= d;
-                else
-                    n.Coordinates.X -= d == 2 ? 1 : -1;
+                if (n.Direction != direction)
+                    n.Change.Add((this._coordinates, this.Direction));
 
                 n.Move();
             });
+
+            this.EatFood();
+        }
+
+        public void EatFood()
+        {
+            // If there's no food here, quit.
+            // If there's, eat it and increase the tale
+            if (!Food.IsThereFoodHere(this._coordinates)) return;
+
+            Food.DisplayedFood = null;
+            this.Grow();
         }
 
         public void Draw()
         {
-            Console.SetCursorPosition(this._coordinates.X, this._coordinates.Y);
-            Console.Write(this);
+            // Drawing every node on screen
+            this._body.ForEach(n =>
+            {
+                Console.SetCursorPosition(n.Coordinates.X, n.Coordinates.Y);
+                Console.Write(n.Char);
+            });
         }
 
-        private void Grow()
+        public void Grow()
         {
-            Node node = new Node(this.HeadDirection);
-            this._body.Add(node);
-        }
+            // Initializing
+            //      * coordinates of the new grown node
+            //      * base coordinates where the new ones will be calculated upon
+            //      * base direction (direction that the new born node should follow)
+            (int X, int Y) coordinates;
+            (int X, int Y) baseCoordinates = this._coordinates;
+            Directions baseDirection = this.Direction;
 
-        public override string ToString()
-        {
-            return "".PadLeft(this.Size, '⬛');
+            // If there's more than one node, set all of the previous vars to its own
+            if (this._body.Count > 0)
+            {
+                baseCoordinates = this._body.Last().Coordinates;
+                baseDirection = this._body.Last().Direction;
+            }
+
+            // Calculating X and Y position of new grown node on the grid
+            if (baseDirection == Directions.Up)
+                coordinates = (baseCoordinates.X, baseCoordinates.Y - Snake.Unit);
+            else if (baseDirection == Directions.Down)
+                coordinates = (baseCoordinates.X, baseCoordinates.Y + Snake.Unit);
+            else if (baseDirection == Directions.Right)
+                coordinates = (baseCoordinates.X - Snake.Unit, baseCoordinates.Y);
+            else
+                coordinates = (baseCoordinates.X + Snake.Unit, baseCoordinates.Y);
+
+            // Creating the new node and adding it to the node list
+            Node n = new Node(this.Direction, coordinates);
+            this._body.Add(n);
+
+            // 50% chance of making the game go faster
+            if (new Random().Next(1, 3) % 2 == 0)
+                this.FrameRate = (int)(this.FrameRate * 0.9);
+
+            // Set the cursor to the position of the new grown node
+            Console.SetCursorPosition(baseCoordinates.X, baseCoordinates.Y);
         }
     }
 }
