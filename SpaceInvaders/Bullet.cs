ddcaad95968cea0e4833e102f7b49debc2f3af59
bored-user <joao.guimaraes.alves.vaz@gmail.com>
2020-10-11 17:53:32 -0300

    Changing foreground color, static char & refacts

--- SpaceInvaders/Bullet.cs
+++ SpaceInvaders/Bullet.cs
@@ -6,28 +6,20 @@ namespace Games.SpaceInvaders
     class Bullet
     {
         private (int X, int Y) _coordinates;
-        private char _char = '▮';
+        private Spaceship _shooter;
         private static List<Bullet> BulletList = new List<Bullet>();
 
-        public Bullet((int X, int Y) coordinates)
+        public Bullet((int X, int Y) coordinates, Spaceship shooter = Spaceship.Player)
         {
             this._coordinates = coordinates;
+            this._shooter = shooter;
             Bullet.BulletList.Add(this);
         }
 
         public static void Move()
         {
-            List<Bullet> toBeRemoved = new List<Bullet>();
-
-            Bullet.BulletList.ForEach(b =>
-            {
-                b._coordinates.Y -= Program.Unit;
-
-                if (b._coordinates.Y <= 0 || b._coordinates.Y >= Console.WindowHeight)
-                    toBeRemoved.Add(b);
-            });
-
-            toBeRemoved.ForEach(b => Bullet.BulletList.Remove(b));
+            Bullet.GarbageCollection();
+            Bullet.BulletList.ForEach(b => b._coordinates.Y += b._shooter == Spaceship.Player ? -Program.Unit : Program.Unit);
         }
 
         public static void Draw()
@@ -35,8 +27,16 @@ namespace Games.SpaceInvaders
             Bullet.BulletList.ForEach(b =>
             {
                 Console.SetCursorPosition(b._coordinates.X, b._coordinates.Y);
-                Console.Write(b._char);
+                Console.ForegroundColor = b._shooter == Spaceship.Invader ? ConsoleColor.Green : Console.ForegroundColor;
+                Console.Write('▮');
+                Console.ForegroundColor = b._shooter == Spaceship.Invader ? ConsoleColor.Green : Console.ForegroundColor;
             });
         }
+
+        public static void GarbageCollection() => Bullet.BulletList
+                                                    .FindAll(b => b._coordinates.Y <= 0 || b._coordinates.Y >= Console.WindowHeight || Bullet.WasHitInvoker(b))
+                                                    .ForEach(b => Bullet.BulletList.Remove(b));
+
+        private static bool WasHitInvoker(Bullet b) => b._shooter != Spaceship.Invader ? Invader.WasHit((b._coordinates.X, b._coordinates.Y)) : Player.WasHit((b._coordinates.X, b._coordinates.Y));
     }
 }
--- SpaceInvaders/Player.cs
+++ SpaceInvaders/Player.cs
@@ -5,29 +5,26 @@ namespace Games.SpaceInvaders
     static class Player
     {
         private static int X = Console.WindowWidth / 2;
+        public static int Y = (int)(Console.WindowHeight * 0.9);
 
         public static void Draw()
         {
-            Console.SetCursorPosition(Player.X, (int)(Console.WindowHeight * 0.9));
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.SetCursorPosition(Player.X, Player.Y);
             Console.Write('■');
-            Console.SetCursorPosition(Player.X + Program.Unit, (int)(Console.WindowHeight * 0.9));
+            Console.SetCursorPosition(Player.X + Program.Unit, Player.Y);
             Console.Write('■');
-            Console.SetCursorPosition(Player.X - Program.Unit, (int)(Console.WindowHeight * 0.9));
+            Console.SetCursorPosition(Player.X - Program.Unit, Player.Y);
             Console.Write('■');
-            Console.SetCursorPosition(Player.X + 2 * Program.Unit, (int)(Console.WindowHeight * 0.9));
+            Console.SetCursorPosition(Player.X + 2 * Program.Unit, Player.Y);
             Console.Write('■');
-            Console.SetCursorPosition(Player.X - 2 * Program.Unit, (int)(Console.WindowHeight * 0.9));
+            Console.SetCursorPosition(Player.X - 2 * Program.Unit, Player.Y);
             Console.Write('■');
         }
 
-        public static void Move(Directions direction)
-        {
-            Player.X += (int)direction;
-        }
+        public static void Move(Directions direction) => Player.X += (int)direction;
+        public static void Shoot() => new Bullet((Player.X, Player.Y - Program.Unit));
+        public static bool WasHit((int X, int Y) coordinates) => (coordinates.X == Player.X || coordinates.X == Player.X - 1 || coordinates.X == Player.X - 2 || coordinates.X == Player.X + 1 || coordinates.X == Player.X + 2) && coordinates.Y == Player.Y;
 
-        public static void Shoot()
-        {
-            Bullet bullet = new Bullet((Player.X, (int)(Console.WindowHeight * 0.9) - Program.Unit));
-        }
     }
 }
