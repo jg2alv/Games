24399e5f907170f92b4f82e2d25080d707e48f68
bored-user <joao.guimaraes.alves.vaz@gmail.com>
2020-10-11 18:32:08 -0300

    Add bullet-bullet collision and changed FG color

--- SpaceInvaders/Bullet.cs
+++ SpaceInvaders/Bullet.cs
@@ -27,16 +27,15 @@ namespace Games.SpaceInvaders
             Bullet.BulletList.ForEach(b =>
             {
                 Console.SetCursorPosition(b._coordinates.X, b._coordinates.Y);
-                Console.ForegroundColor = b._shooter == Spaceship.Invader ? ConsoleColor.Green : Console.ForegroundColor;
+                Console.ForegroundColor = b._shooter == Spaceship.Invader ? ConsoleColor.Green : ConsoleColor.White;
                 Console.Write('▮');
-                Console.ForegroundColor = b._shooter == Spaceship.Invader ? ConsoleColor.Green : Console.ForegroundColor;
             });
         }
 
         public static void GarbageCollection() => Bullet.BulletList
-                                                    .FindAll(b => b._coordinates.Y <= 0 || b._coordinates.Y >= Console.WindowHeight || Bullet.WasHitInvoker(b))
+                                                    .FindAll(b => b._coordinates.Y <= 0 || b._coordinates.Y >= Console.WindowHeight || Bullet.WasHit(b))
                                                     .ForEach(b => Bullet.BulletList.Remove(b));
 
-        private static bool WasHitInvoker(Bullet b) => b._shooter != Spaceship.Invader ? Invader.WasHit((b._coordinates.X, b._coordinates.Y)) : Player.WasHit((b._coordinates.X, b._coordinates.Y));
+        private static bool WasHit(Bullet b) => (b._shooter != Spaceship.Invader ? Invader.WasHit((b._coordinates.X, b._coordinates.Y)) : Player.WasHit((b._coordinates.X, b._coordinates.Y))) && Bullet.BulletList.FindAll(_b => b._coordinates.X == _b._coordinates.X && b._coordinates.Y == _b._coordinates.Y && b._shooter != _b._shooter).Count != 0;
     }
 }
