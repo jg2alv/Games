5ddc284ceb526974daefe2bbb9533e33c4fb59b6
bored-user <joao.guimaraes.alves.vaz@gmail.com>
2020-11-08 20:15:51 -0300

    Implemented pawn movement & capture mechanism

--- Chess/Piece.cs
+++ Chess/Piece.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 
 namespace Games.Chess
 {
@@ -16,7 +17,60 @@ namespace Games.Chess
             this.Position = position;
         }
 
-        public static (int row, int col) GetPosition(Piece piece) => (piece.Position.x, Array.FindIndex(new string[] { "a", "b", "c", "d", "e", "f", "g", "h" }, 0, 8, l => l == piece.Position.y));
+        public static (int row, int col) GetPosition(Piece piece) => (piece.Position.x, Array.FindIndex(Program.Cols, 0, 8, l => l == piece.Position.y));
+
+        public Piece[] CanGoTo()
+        {
+            List<Piece> houses = new List<Piece>();
+            (int x, int y) position = (this.Position.x, Array.FindIndex(Program.Cols, 0, 8, l => l == this.Position.y));
+            int unit = (int)this.Owner;
+
+            switch (this.Type)
+            {
+                case Pieces.P:
+                    // Checking house ahead & both diagonals
+                    (int x, string y) nextHouse = (position.x + unit, Program.Cols[position.y]);
+                    (int x, string y)[] nextDiagonalHouse = position.y == 0 ? new (int x, string y)[] { (position.x + unit, Program.Cols[position.y + 1]) } : position.y == 7 ? new (int x, string y)[] { (position.x + unit, Program.Cols[position.y - 1]) } : new (int x, string y)[] { (position.x + unit, Program.Cols[position.y + 1]), (position.x + unit, Program.Cols[position.y - 1]) };
+
+                    if (Board.IsEmpty(nextHouse))
+                        houses.Add(Board.GetPiece(nextHouse));
+
+                    foreach ((int x, string y) _nextDiagonalHouse in nextDiagonalHouse)
+                    {
+                        if (Board.Owner(_nextDiagonalHouse) == this.Owner || Board.Owner(_nextDiagonalHouse) == Players.Empty) continue;
+                        houses.Add(Board.GetPiece(_nextDiagonalHouse));
+                    }
+
+                    break;
+
+                case Pieces.R:
+                    break;
+
+                case Pieces.N:
+                    break;
+
+                case Pieces.B:
+                    break;
+
+                case Pieces.Q:
+                    break;
+
+                case Pieces.K:
+                    break;
+            }
+
+            return houses.ToArray();
+        }
+
+        public void Capture(Piece captured)
+        {
+            captured.Owner = this.Owner;
+            captured.Type = this.Type;
+
+            this.Selected = false;
+            this.Owner = Players.Empty;
+            this.Type = Pieces.E;
+        }
 
         public override string ToString() => this.Type == Pieces.E ? " " : Enum.GetName(typeof(Pieces), this.Type);
     }
