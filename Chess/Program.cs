fdbc34204ba01e47845c922e47b13f2dc5adb14e
bored-user <joao.guimaraes.alves.vaz@gmail.com>
2020-11-08 20:16:48 -0300

    Adaptations to the two prior commits

--- Chess/Program.cs
+++ Chess/Program.cs
@@ -6,22 +6,30 @@ namespace Games.Chess
     class Program
     {
         public static Players Player = Players.White;
+        public static int[] Rows = new int[] { 0, 1, 2, 3, 4, 5, 6, 7 };
+        public static string[] Cols = new string[] { "a", "b", "c", "d", "e", "f", "g", "h" };
 
         static void Main()
         {
-            Board board = new Board();
+            Board.BuildBoard();
 
             while (true)
             {
                 Console.WriteLine($"It's {Enum.GetName(typeof(Players), Program.Player).ToUpper()}'s turn!\n");
                 Regex regex = new Regex(@"^[a-h][0-8]$");
 
-                board.Draw();
-                Piece from = board.GetInput("> Input house on RowCol format: ", regex, false);
+                Board.Draw();
+                Piece from = Board.GetInput("> Input house on RowCol format: ", regex, false);
                 Console.Clear();
-                board.Draw();
-                Piece to = board.GetInput("> Input house to land selected piece on: ", regex, true, false);
-                Board.CheckAndPlay(from, to);
+                Board.Draw();
+                while (true)
+                {
+                    Piece to = Board.GetInput("> Input house to land selected piece on: ", regex, true, false);
+                    if (Board.Play(from, to)) break;
+                    Console.WriteLine("Please, selected a playable piece/ house.");
+                }
+
+
 
                 Program.Player = Program.Player == Players.White ? Players.Black : Players.White;
                 Console.Clear();
@@ -32,7 +40,7 @@ namespace Games.Chess
         {
             (int row, int col) position = Piece.GetPosition(piece);
 
-            Console.ForegroundColor = piece.Owner == 0 ? ConsoleColor.DarkMagenta : ConsoleColor.DarkRed;
+            Console.ForegroundColor = piece.Owner == Players.White ? ConsoleColor.DarkMagenta : ConsoleColor.DarkRed;
             Console.BackgroundColor = piece.Selected ? ConsoleColor.Yellow : position.row % 2 == 0 ? (position.col % 2 == 0 ? ConsoleColor.Gray : ConsoleColor.DarkGray) : (position.col % 2 == 0 ? ConsoleColor.DarkGray : ConsoleColor.Gray);
         }
     }
